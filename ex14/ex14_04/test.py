c06dc7871248f78e881b645f326abb5c3d431dbe
tclintaichung <tclin@nchu.edu.tw>
2026-08-30 11:29:27 +0800

    Delete ex14/ex14_04/test.py

--- ex14/ex14_04/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-          
-dataset_id = "d_3d227e5d9fdec73f3bcadce671c333a6"
-url = "https://data.gov.sg/api/action/datastore_search?resource_id="  + dataset_id
-        
-response = requests.get(url)
-print(response.json())
