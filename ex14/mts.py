38954e87374a926839de0bacc78990acf8264c23
tclintaichung <tclin@nchu.edu.tw>
2026-08-30 11:30:45 +0800

    Delete ex14/mts.py

--- ex14/mts.py
+++
@@ -1,9 +0,0 @@
-# from js import document
-# from pyscript import create_proxy
-
-print("Hi")
-def on_click(event):
-  print("Hello")
-
-# button = document.getElementById("button")
-# button.addEventListener("click", create_proxy(on_click))
