8cc3ed5c0de2365df94772d3b6a9f03751af8391
tclintaichung <tclin@nchu.edu.tw>
2026-09-12 10:19:42 +0800

    Add files via upload

--- 20260913/mymap.js
+++ 20260913/mymap.js
@@ -8,3 +8,13 @@ myMap.hello = function() {
     console.log("Hello from myMap!");
 };
 myMap.hello();
+myNewMap = new Map();
+myNewMap.set("book", 10);
+myNewMap.set("milk", 5);
+console.log(myNewMap.get("book"));
+console.log(myNewMap.get("milk"));
+
+myNewMap.hello = function() {
+    console.log("Hello from myNewMap!");
+};
+myNewMap.hello();
